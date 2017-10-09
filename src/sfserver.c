a858a4e9621b5549e80bbb7d0e7e02968ffdfdb2
deviantfero <fmorataya.04@gmail.com>
2017-10-08 21:05:25 -0600

    update sfserver, add option parsing

--- src/sfserver.c
+++ src/sfserver.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
-#include "./sfserver.h"
+#include <unistd.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "./consts.h"
+#include "./logger.h"
+#include "./status.h"
 
-int main(int argc, char *argv[])
-{
-	printf("hello world\n");
+int main(int argc, char *argv[]) {
+	const char *dir = DEFAULT_DIR;
+	bool debug = true;
+	int opt;
+
+	while((opt = getopt(argc, argv, "sD:")) != -1) {
+		switch(opt) {
+			case 'D': 
+				if((dir = realpath(optarg, NULL)) == NULL) {
+					fprintf(stderr, "%s: No such path or directory.\n", argv[0]);
+					exit(2);
+				}
+				break;
+			case 's': debug = false; break;
+			case '?':
+				if(opt == 'D')
+					fprintf(stderr, "-%c requires an argument.\n", opt);
+				break;
+			default:
+				dir = realpath(DEFAULT_DIR, NULL);
+				break;
+		}
+	}
+	fprintf(stdout, "I'm waiting at pid: %d, serving: %s\n", getpid(), dir);
+	output_dir_contents(dir);
 	return 0;
 }
