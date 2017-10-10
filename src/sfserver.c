95dda1b3ff0233db16011a6b1ceeeca62c69300e
deviantfero <fmorataya.04@gmail.com>
2017-10-09 18:46:09 -0600

    wait for client message, more through option parsing

--- src/sfserver.c
+++ src/sfserver.c
@@ -1,16 +1,24 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <limits.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include "./consts.h"
-#include "./logger.h"
-#include "./status.h"
+#include <string.h>
+#include "consts.h"
+#include "logger.h"
+#include "status.h"
+
+#define MAX_BUFFER 4096
+#define WAIT_TIME  5
 
 int main(int argc, char *argv[]) {
 	const char *dir = DEFAULT_DIR;
+	const char *fifo_path = "/tmp/fifo";
 	bool debug = true;
-	int opt;
+	char *msg_buffer = malloc(MAX_BUFFER);
+	int opt, fifod;
+	struct directory *current_dir;
 
 	while((opt = getopt(argc, argv, "sD:")) != -1) {
 		switch(opt) {
@@ -22,15 +30,26 @@ int main(int argc, char *argv[]) {
 				break;
 			case 's': debug = false; break;
 			case '?':
-				if(opt == 'D')
+				if(opt == 'D') {
 					fprintf(stderr, "-%c requires an argument.\n", opt);
+				}
 				break;
 			default:
-				dir = realpath(DEFAULT_DIR, NULL);
+				if((dir = realpath(DEFAULT_DIR, NULL)) == NULL) {
+					fprintf(stderr, "%s: No such path or directory.\n", argv[0]);
+					exit(2);
+				}
 				break;
 		}
 	}
-	fprintf(stdout, "I'm waiting at pid: %d, serving: %s\n", getpid(), dir);
-	output_dir_contents(dir);
+
+	fprintf(stdout, "[pid: %d][dir: %s]\nwaiting clients...\n", getpid(), dir);
+
+	for(;;) {
+		fifod = open(fifo_path, O_RDONLY);
+		if((read(fifod, msg_buffer, MAX_BUFFER)) != EOF)
+			printf("%s\n", msg_buffer);
+		close(fifod);
+	}
 	return 0;
 }
