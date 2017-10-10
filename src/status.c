db7fc78038cfdf6313b8316ed9483d2e74725083
deviantfero <fmorataya.04@gmail.com>
2017-10-09 18:48:50 -0600

    add status functions that return the directories content

--- src/status.c
+++ src/status.c
@@ -2,12 +2,50 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <dirent.h>
+#include <string.h>
+#include "status.h"
 
-void output_dir_contents(const char* dir) {
+void print_dir_contents(const char *dir) {
 	DIR *fd = opendir(dir);
 	struct dirent *dir_contents;
 	while((dir_contents = readdir(fd)) != NULL) {
-		if(dir_contents->d_type == DT_DIR || dir_contents->d_type == DT_REG)
+		if(dir_contents->d_type == DT_DIR || dir_contents->d_type == DT_REG) {
 			fprintf(stdout, "%s\n", dir_contents->d_name);
+		}
 	}
+}
+
+void fprint_dir_contents(const char *dir, FILE* file) {
+	DIR *fd = opendir(dir);
+	struct dirent *dir_contents;
+	while((dir_contents = readdir(fd)) != NULL) {
+		if(dir_contents->d_type == DT_DIR || dir_contents->d_type == DT_REG) {
+			fprintf(file, "%s\n", dir_contents->d_name);
+		}
+	}
+}
+
+struct directory *get_dir_contents(const char *dir) {
+	struct dirent *dir_contents;
+	struct directory *current_dir = malloc(sizeof(struct directory));
+	current_dir->contents = malloc(sizeof(char*));
+
+	if(current_dir == NULL)
+		return NULL;
+
+	DIR *fd = opendir(dir);
+
+	for(int i = 0; (dir_contents = readdir(fd)) != NULL;){
+		if(dir_contents->d_type == DT_DIR || dir_contents->d_type == DT_REG) {
+			current_dir->contents = realloc(current_dir->contents, sizeof(char*) * (i + 1));
+			current_dir->contents[i] = dir_contents->d_name;
+			current_dir->file_count = i + 1;
+
+			if(current_dir->contents[i] == NULL) printf("null at %d\n", i);
+			i++;
+		}
+
+	}
+
+	return current_dir;
 }
--- src/status.h
+++ src/status.h
@@ -1,6 +1,22 @@
 #ifndef STATUS_H
 #define STATUS_H
 
-void output_dir_contents(const char* dir);
+struct directory {
+	char **contents;
+	int file_count;
+};
+
+/* prints a directory's content to stdout
+ * receives the directory path as argument */
+void print_dir_contents(const char* dir);
+
+/* prints a directory's content to specified file
+ * receives the directory path as argument */
+void fprint_dir_contents(const char* dir, FILE* file);
+
+/* returns an array containing the names of
+ * the directory's contents receives the 
+ * directory path as argument */
+struct directory *get_dir_contents(const char* dir);
 
 #endif /* STATUS_H defined */
