8d4a37e04f352ca77133c0ff99781987111e5582
deviantfero <fmorataya.04@gmail.com>
2017-11-04 02:25:48 -0600

    remove useless comments, send message to clients, handle status and ls signals properly

--- src/sfserver.c
+++ src/sfserver.c
@@ -24,6 +24,8 @@ int main(int argc, char *argv[]) {
 	int opt;
 	status = (struct server_status*) malloc(sizeof(struct server_status));
 	status->dir = DEFAULT_DIR;
+	status->downloads = 0;
+	status->uploads = 0;
 
 	while((opt = getopt(argc, argv, "D:")) != -1) {
 		switch(opt) {
@@ -55,7 +57,6 @@ int main(int argc, char *argv[]) {
 
 	char** msg = malloc(sizeof(char*) * 2);
 
-	// initializing pointer, good practice (?)
 	pthread_t* client_threads = malloc(sizeof(pthread_t)); 
 
 	for(int i = 0;;) {
@@ -85,28 +86,46 @@ int main(int argc, char *argv[]) {
 
 void *client_handler(void *param_msg) {
 	char** msg = (char**)param_msg;
-	char* cpipe_name = malloc(sizeof(MAX_BUFFER));
-
-	if(cpipe_name == NULL) {
-		fprintf(stderr, "Error: in allocating cpipe_name\n");
-		pthread_exit(NULL);
-	}
+	size_t name_len = 0;
+	char* rpipe_name;
+	char* wpipe_name;
 
 	pthread_mutex_lock(&cc_mutex);
 	status->client_count++;
 	fprint_status(stdout, status);
 	pthread_mutex_unlock(&cc_mutex);
 
+	/* same length is true for both pipe's names */
+	name_len = buffer_size("/tmp/sfc%sw", msg[SENDER]);
+
+	rpipe_name = malloc(name_len);
+	wpipe_name = malloc(name_len);
+
+	if(rpipe_name == NULL || wpipe_name == NULL) {
+		fprintf(stdout, "error allocating either rpipe or wpipe names\n");
+		pthread_exit(msg);
+	}
+
 	/* we read where the client writes, thus we read from sfc(pid)w */
-	snprintf(cpipe_name, MAX_BUFFER, "/tmp/sfc%sw", msg[SENDER]);
+	/* we wirte where the client reads, thus we write in sfc(pid)r */
+	snprintf(wpipe_name, name_len, "/tmp/sfc%sr", msg[SENDER]);
+	snprintf(rpipe_name, name_len, "/tmp/sfc%sw", msg[SENDER]);
 
 	while(1) {
 		sleep(WAIT_TIME);
-		msg = wait_message(cpipe_name);
-		/* handle messages */
+		msg = wait_message(rpipe_name);
+
 		if(strncmp(msg[SIGNAL], MSG_LS, sizeof(MSG_LS)) == 0) {
 			fprintf(stdout, "handling ls signal (%s)\n", msg[SENDER]);
-		} else if(strncmp(msg[SIGNAL], MSG_EXIT, sizeof(MSG_LS)) == 0) {
+			char* files_msg = sprint_dir_status(files_msg, status);
+			send_message(wpipe_name, files_msg, true);
+			free(files_msg);
+		} else if(strncmp(msg[SIGNAL], MSG_STATUS, sizeof(MSG_STATUS)) == 0){
+			fprintf(stdout, "handling status signal (%s)\n", msg[SENDER]);
+			char* status_msg = sprint_status(status_msg, status);
+			send_message(wpipe_name, status_msg, true);
+			free(status_msg);
+		} else if(strncmp(msg[SIGNAL], MSG_EXIT, sizeof(MSG_EXIT)) == 0) {
 			fprintf(stdout, "closing thread for (%s)...\n", msg[SENDER]);
 
 			pthread_mutex_lock(&cc_mutex);
@@ -118,6 +137,7 @@ void *client_handler(void *param_msg) {
 		}
 	}
 
-	free(cpipe_name);
+	free(wpipe_name);
+	free(rpipe_name);
 	pthread_exit(msg);
 }
