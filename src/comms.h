3a9d89aa7a0c47d60b0587c2c55ca165ed559d02
deviantfero <fmorataya.04@gmail.com>
2017-11-18 03:59:41 -0600

    make MAX_BUFFER same as in client, add new messages, encrypt, chunk size

--- src/comms.h
+++ src/comms.h
@@ -1,5 +1,7 @@
 #ifndef COMMS_H
 
+#define COMMS_H
+
 #include <string.h>
 #include <fcntl.h>
 #include <limits.h>
@@ -12,23 +14,38 @@
 
 #include "utils.h"
 
-#define COMMS_H
 #define SIGNAL 0
 #define SENDER 1
 #define WAIT_TIME 1000
 #define NO_TIMEOUT 0
 #define MSG_TIMEOUT "TIMEOUT"
 #define DFT_TRIES 10 // default number of tries
-#define MAX_BUFFER 1024
+#define MAX_BUFFER 4096
 #define MAX_TOKENS 10
-#define MSG_METHOD "method"
-#define MSG_LS     "ls"
-#define MSG_STATUS "status"
-#define MSG_DONE   "done"
-#define MSG_UPLD   "upload"
-#define MSG_DOWNLD "download"
-#define MSG_EXIT   "bye"
-#define MSG_ARRIVE "hello"
+#define MSG_ENCRYPT   "encrypt"
+#define MSG_CHUNKSIZE "chunksize"
+#define MSG_METHOD    "method"
+#define MSG_LS        "ls"
+#define MSG_STATUS    "status"
+#define MSG_DONE      "done"
+#define MSG_UPLD      "upload"
+#define MSG_DOWNLD    "download"
+#define MSG_EXIT      "bye"
+#define MSG_ARRIVE    "hello"
+
+enum method {
+	PIPES,
+	QUEUE,
+	SOCKETS
+};
+
+struct options {
+	int chunksize;
+	int pid;
+	enum method m;
+	bool encrypt;
+	bool compress;
+};
 
 /* messages should follow format "what;pid" */
 void send_message(const char *pipe_name, char *msg, bool do_unlink);
