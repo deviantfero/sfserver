97d698e19e461fb95cfb5ee3179aeb70d388787c
deviantfero <fmorataya.04@gmail.com>
2017-11-21 20:13:37 -0600

    add compression and decompression support

--- src/utils.c
+++ src/utils.c
@@ -17,3 +17,51 @@ void encrypt(char *message, char * key, ssize_t chunksize) {
         message[i] = message[i] ^ key[i % keylen];
     }
 }
+
+char* deflate_file(char *src) {
+	int src_fd = open(src, O_RDONLY);
+	ssize_t filesize = lseek(src_fd, 0, SEEK_END);
+	lseek(src_fd, (off_t) 0, SEEK_SET);
+    ssize_t gznamesize = buffer_size("%s.gz", src);
+    char *gzsrc  = malloc(gznamesize);
+    snprintf(gzsrc, gznamesize, "%s.gz", src);
+
+    char *file = malloc(filesize + 1);
+    struct gzFile_s *fi = (struct gzFile_s *)gzopen(gzsrc, "wb");
+    read(src_fd, file, filesize);
+
+    gzwrite(fi, file, filesize);
+    gzclose(fi);
+
+    free(file);
+    return gzsrc;
+}
+
+ssize_t inflate_file(char *src, bool del) {
+    int usrcsize = buffer_size("%s", src) - 4;
+    char buffer[MAX_BUFFER];
+
+    char *usrc = malloc(usrcsize);
+    memset(usrc, 0, usrcsize);
+
+    strncpy(usrc, src, usrcsize);
+    usrc[usrcsize] = '\0';
+
+    struct gzFile_s *fi = (struct gzFile_s *)gzopen(src, "rb");
+    gzrewind(fi);
+
+
+    int fd  = open(usrc, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+    int len = 0; 
+    ssize_t total = 0;
+    while(!gzeof(fi)) {
+        len = gzread(fi, buffer, sizeof(buffer));
+        total += len;
+        write(fd, buffer, len);
+    }
+
+    gzclose(fi);
+    close(fd);
+    if(del) unlink(src);
+    return total;
+}
--- src/utils.h
+++ src/utils.h
@@ -9,9 +9,14 @@
 #include <fcntl.h>
 #include <stdarg.h>
 #include <stdio.h>
-#include "utils.h"
+#include <zlib.h>
+#include <stdbool.h>
+
+#include "comms.h"
 
 int buffer_size(const char* format, ...);
-void encrypt(char *message, char * key, ssize_t chunksize);
+void encrypt(char *message, char *key, ssize_t chunksize);
+ssize_t inflate_file(char *src, bool del);
+char *deflate_file(char *src);
 
 #endif
